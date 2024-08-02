98d81b922feac133b0f688df460aec23178d2c5c
Hamza1669 <hamzasajjad222@gmail.com>
2024-08-02 17:13:23 +0500

    domain classes for student and admission

---
+++ src/main/java/com/student/model/Admission.java
@@ -0,0 +1,11 @@
+package com.student.model;
+
+public class Admission {
+	
+	private Long id;
+	private Date admissionDate;
+    private String status;
+	
+	
+
+}
---
+++ src/main/java/com/student/model/Student.java
@@ -0,0 +1,13 @@
+package com.student.model;
+import java.util.Date;
+
+
+public class Student {
+	
+	    private Long id;
+	    private String firstName;
+	    private String lastName;
+	    private String email;
+	    private Date dateOfBirth;
+
+}
