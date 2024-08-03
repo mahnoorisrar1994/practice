2ce0447675f74a7d433380b9084142895c302b58
Hamza1669 <hamzasajjad222@gmail.com>
2024-08-03 17:52:36 +0500

    make model class into Entity

--- src/main/java/com/student/model/Student.java
+++ src/main/java/com/student/model/Student.java
@@ -2,8 +2,16 @@ package com.student.model;
 
 import java.util.Objects;
 
-public class Student {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
+public class Student {
+ 
+	    @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
 		private Long id;
 	    private String firstName;
 	    private String lastName;
