c4e303c8fdc434c13d160d9b9548c8a84e3afd16
Hamza1669 <hamzasajjad222@gmail.com>
2024-08-16 00:35:46 +0200

    Test for GET GET end-point api/allStudents/{id}

--- src/main/java/com/student/controllers/StudentRestController.java
+++ src/main/java/com/student/controllers/StudentRestController.java
@@ -3,6 +3,7 @@ package com.student.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.student.model.Student;
@@ -21,5 +22,8 @@ public class StudentRestController {
 	return studentService.readAllStudents();
 	}
 	
-
+	@GetMapping("/api/students/{id}")
+	public Student student(@PathVariable long id) {
+	return studentService.findStudentById(id);
+	}
 }
--- src/test/java/com/student/controllers/StudentRestControllerTest.java
+++ src/test/java/com/student/controllers/StudentRestControllerTest.java
@@ -1,6 +1,7 @@
 package com.student.controllers;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -9,8 +10,10 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import java.time.LocalDate;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -22,6 +25,7 @@ import com.student.model.Admission;
 import com.student.model.Student;
 import com.student.services.StudentService;
 
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
 
 import static java.util.Arrays.asList;
 
@@ -35,6 +39,13 @@ class StudentRestControllerTest {
 	@MockBean
 	private StudentService studentService;
 
+	@BeforeEach
+	public void setUp() {
+		// Initialize the MockMvc instance for RestAssuredMockMvc
+		RestAssuredMockMvc.mockMvc(mvc);
+		MockitoAnnotations.openMocks(this);
+	}
+
 	@Test
 	void testAllStudentsEmpty() throws Exception {
 		this.mvc.perform(get("/api/allStudents").accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
@@ -47,13 +58,31 @@ class StudentRestControllerTest {
 		Admission firstAdmission = new Admission(1L, LocalDate.of(2021, 02, 2), "pending");
 		Admission secondAdmission = new Admission(2L, LocalDate.of(2021, 10, 2), "approved");
 		when(studentService.readAllStudents())
-				.thenReturn(asList(new Student(1L, "Hamza", "Khan", "Hamzakhan@gmail.com", firstAdmission), new Student(2L, "Hamza", "Khan", "Hamzakhan@gmail.com", secondAdmission)));
+				.thenReturn(asList(new Student(1L, "Hamza", "Khan", "Hamzakhan@gmail.com", firstAdmission),
+						new Student(2L, "Hamza", "Khan", "Hamzakhan@gmail.com", secondAdmission)));
 		this.mvc.perform(get("/api/allStudents").accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
-		.andExpect(jsonPath("$[0].id", is(1))).andExpect(jsonPath("$[0].firstName", is("Hamza")))
-		.andExpect(jsonPath("$[0].lastName", is("Khan"))).andExpect(jsonPath("$[0].email", is("Hamzakhan@gmail.com")))
-		.andExpect(jsonPath("$[1].id", is(2)))
-		.andExpect(jsonPath("$[1].firstName", is("Hamza"))).andExpect(jsonPath("$[0].lastName", is("Khan")))
-		.andExpect(jsonPath("$[1].email", is("Hamzakhan@gmail.com")));
-}
+				.andExpect(jsonPath("$[0].id", is(1))).andExpect(jsonPath("$[0].firstName", is("Hamza")))
+				.andExpect(jsonPath("$[0].lastName", is("Khan")))
+				.andExpect(jsonPath("$[0].email", is("Hamzakhan@gmail.com"))).andExpect(jsonPath("$[1].id", is(2)))
+				.andExpect(jsonPath("$[1].firstName", is("Hamza"))).andExpect(jsonPath("$[0].lastName", is("Khan")))
+				.andExpect(jsonPath("$[1].email", is("Hamzakhan@gmail.com")));
+	}
 
+	@Test
+	void test_OneStudentById_WithExistingStudent() throws Exception {
+		Admission firstAdmission = new Admission(1L, LocalDate.of(2021, 02, 2), "pending");
+		when(studentService.findStudentById(anyLong()))
+				.thenReturn(new Student(1L, "Hamza", "Khan", "Hamzakhan@gmail.com", firstAdmission));
+		this.mvc.perform(get("/api/students/1").accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+				.andExpect(jsonPath("$.id", is(1))).andExpect(jsonPath("$.firstName", is("Hamza")))
+				.andExpect(jsonPath("$.lastName", is("Khan")))
+				.andExpect(jsonPath("$.email", is("Hamzakhan@gmail.com")));
+	}
+
+	@Test
+	void test_OneStudentById_WithNotFoundStudent() throws Exception {
+		when(studentService.findStudentById(anyLong())).thenReturn(null);
+		this.mvc.perform(get("/api/students/1").accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+				.andExpect(content().string(""));
+	}
 }
