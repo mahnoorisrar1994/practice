faf7c8dac4c87c67296ec1f97131a50f018a25af
Hamza1669 <hamzasajjad222@gmail.com>
2024-08-18 03:49:19 +0200

    Test for @GetMapping("/")

--- src/main/java/com/student/controllers/StudentWebController.java
+++ src/main/java/com/student/controllers/StudentWebController.java
@@ -1,14 +1,27 @@
 package com.student.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.student.model.Student;
+import com.student.services.StudentService;
 
 @Controller
 public class StudentWebController {
 
-	@GetMapping("/")
-	public String index() {
-		return "index";
-	}
+	@Autowired
+	private StudentService studentService;
 
+	@GetMapping("/")
+	public String index(Model model) {
+		List<Student> allStudents = studentService.readAllStudents();
+		model.addAttribute("students", allStudents);
+		model.addAttribute("message", allStudents.isEmpty() ? "No student detail present" : "");
+		return "index";
+
+	}
 }
--- src/test/java/com/student/controllers/StudentWebControllerTest.java
+++ src/test/java/com/student/controllers/StudentWebControllerTest.java
@@ -1,14 +1,28 @@
 package com.student.controllers;
 
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.ModelAndViewAssert;
 import org.springframework.test.web.servlet.MockMvc;
+
+import com.student.model.Admission;
+import com.student.model.Student;
+import com.student.services.StudentService;
 
 @ExtendWith(MockitoExtension.class)
 @WebMvcTest(controllers = StudentWebController.class)
@@ -17,9 +31,39 @@ class StudentWebControllerTest {
 	@Autowired
 	private MockMvc mvc;
 
+	@MockBean
+	private StudentService studentService;
+
 	@Test
-	void testStatus200() throws Exception {
+	void test_Status200() throws Exception {
 		mvc.perform(get("/")).andExpect(status().is2xxSuccessful());
 	}
+
+	@Test
+	void test_ReturnHomeView() throws Exception {
+		ModelAndViewAssert.assertViewName(mvc.perform(get("/")).andReturn().getModelAndView(), "index");
+
+	}
+
+	@Test
+	void test_HomeView_ShowStudents() throws Exception {
+		Admission firstAdmission = new Admission(1L, LocalDate.of(2021, 2, 2), "pending");
+		Student student = new Student(1L, "Hamza", "Khan", "Hamzakhan@gmail.com", firstAdmission);
+		List<Student> students = Arrays.asList(student);
+
+		when(studentService.readAllStudents()).thenReturn(students);
+
+		mvc.perform(get("/")).andExpect(view().name("index")).andExpect(model().attribute("students", students))
+				.andExpect(model().attribute("message", ""));
+	}
+
+	@Test
+	void test_HomeView_ShowsMessageWhenThereAreNoEmployees() throws Exception {
+		when(studentService.readAllStudents()).thenReturn(Collections.emptyList());
+		mvc.perform(get("/")).andExpect(view().name("index"))
+				.andExpect(model().attribute("students", Collections.emptyList()))
+				.andExpect(model().attribute("message", "No student detail present"));
+	}
+	
 
 }
