1d4c825497a44e0ea20ae95be1c993dfa8ecc55c
Hamza1669 <hamzasajjad222@gmail.com>
2024-08-28 12:45:18 +0200

    test_HomePage E2E

--- src/e2e/test/com/student/StudentWebControllerE2E.java
+++ src/e2e/test/com/student/StudentWebControllerE2E.java
@@ -1,34 +1,47 @@
 package com.student;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 class StudentWebControllerE2E {
-	
-	
 
-	
+	private static int port = Integer.parseInt(System.getProperty("server.port", "8080"));
+	private static String baseUrl = "http://localhost:" + port;
+	private static WebDriver driver;
+
+	@BeforeAll
+	public static void setupClass() {
+		// setup Chrome Driver
+		WebDriverManager.chromedriver().setup();
+	}
+
+	@BeforeEach
+	public void setup() {
+		baseUrl = "http://localhost:" + port;
+		driver = new ChromeDriver();
+	}
+
+	@AfterAll
+	public static void teardown() {
+		if (driver != null) {
+			driver.quit();
+		}
+	}
+
 	@Test
-    void testChromeDriverSetup() {
-  
-        WebDriverManager.chromedriver().setup();
-        
-        WebDriver driver = new ChromeDriver();
-   
-        driver.get("https://www.google.com");
-        
+	void test_HomePage() {
+		driver.get(baseUrl);
+		driver.findElement(By.cssSelector("a[href*='/new"));
+	}
 
-        System.out.println("Page title is: " + driver.getTitle());
- 
-        driver.quit();
-    }
-
-
-
-
+	
+	   
 
 }
