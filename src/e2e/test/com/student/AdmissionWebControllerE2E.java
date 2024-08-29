b29fe65dc562c5d4f15761e89bec8d7646b2a994
Hamza1669 <hamzasajjad222@gmail.com>
2024-08-29 11:16:18 +0200

    Implement wait

--- src/e2e/test/com/student/AdmissionWebControllerE2E.java
+++ src/e2e/test/com/student/AdmissionWebControllerE2E.java
@@ -8,15 +8,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
+import java.util.concurrent.TimeUnit;
+
 
 public class AdmissionWebControllerE2E {
-	
+
 	private static int port = Integer.parseInt(System.getProperty("server.port", "8080"));
 	private static String baseUrl = "http://localhost:" + port;
 	private static WebDriver driver;
@@ -31,6 +29,10 @@ public class AdmissionWebControllerE2E {
 	public void setup() {
 		baseUrl = "http://localhost:" + port;
 		driver = new ChromeDriver();
+
+		// Set the implicit wait time to 10 seconds
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+
 	}
 
 	@AfterAll
@@ -40,20 +42,20 @@ public class AdmissionWebControllerE2E {
 		}
 	}
 
-	@Test
-	void test_CreateAdmission() {
-		// Create a new Admission
-		driver.get(baseUrl + "/newAdmission");
-		driver.findElement(By.name("admissionDate")).sendKeys("29-08-2024");
-		driver.findElement(By.name("status")).sendKeys("Approved");
-		driver.findElement(By.name("course")).sendKeys("Masters");
-		driver.findElement(By.name("btn_submit")).click();
+	  @Test
+	    void test_CreateAdmission() {
+	        // Navigate to the new admission page
+	        driver.get(baseUrl + "/newAdmission");
 
-		assertThat(driver.findElement(By.id("admission_record")).getText()).
-		contains("2024-08-29","Approved", "Masters");
+	        // Fill out the form
+	        driver.findElement(By.name("admissionDate")).sendKeys("29-08-2024");
+	        driver.findElement(By.name("status")).sendKeys("Approved");
+	        driver.findElement(By.name("course")).sendKeys("Masters");
+	        driver.findElement(By.name("btn_submit")).click();
 
-	
-	}
-	
+	        // Assert that the admission record contains the expected values
+	        assertThat(driver.findElement(By.id("admission_record")).getText())
+	            .contains("2024-08-29", "Approved", "Masters");
+	    }
 
 }
